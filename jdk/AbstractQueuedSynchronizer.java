d2791e7dcbfe1dc3302772c29119674f51fdd3e8
zheng <765324639@qq.com>
2018-05-20 15:19:40 +0800

    AQS_src

---
+++ jdk/AbstractQueuedSynchronizer.java
@@ -0,0 +1,652 @@
+/**
+ * java.version = 1.8.0_144
+ */
+
+public abstract class AbstractQueuedSynchronizer {
+
+    private transient volatile Node head;
+    private transient volatile Node tail;
+
+    private volatile int state;
+
+    protected final int getState() {
+        return state;
+    }
+
+    protected final void setState(int newState) {
+        state = newState;
+    }
+
+    protected final boolean compareAndSetState(int expect, int update) {
+        return unsafe.compareAndSwapInt(this, stateOffset, expect, update);
+    }
+
+    static final long spinForTimeoutThreshold = 1000L;
+
+    /**
+     * 插入节点到链表中
+     * 如果链表为空则初始化插入
+     * 使用尾插法插入，并返回之前的尾节点
+     */
+    private Node enq(final Node node) {
+        for(;;) {
+            Node t = tail;
+            if (t = null) {
+                if (compareAndSetHead(new Node())) {
+                    tail = head;
+                }
+            } else {
+                node.prev = t;
+                if (compareAndSetTail(t, node)) {
+                    t.next = node;
+                    return t;
+                }
+            }
+        }
+    }
+
+    /**
+     * 插入节点
+     * 如果链表不为空则插入到尾部，并返回此节点
+     * 如果链表为空则初始化并插入
+     */
+    private Node addWaiter(Node mode) {
+        Node node = new Node(Thread.currentThread(), mode);
+        Node pred = tail;
+        if (pred != null) {
+            node.prev = pred;
+            if (compareAndSetTail(pred, node)) {
+                pred.next = node;
+                return node;
+            }
+        }
+        enq(node);
+        return node;
+    }
+
+    /**
+     * 将头指针指向当前节点
+     * 清空节点内的线程属性
+     */
+    private void setHead(Node node) {
+        head = node;
+        node.thread = null;
+        node.prev = null
+    }
+
+    /**
+     * 取消下一个节点中线程的阻塞状态，让其运行
+     * 如果下一个节点不存在或处于取消状态
+     * 则从tail向前查找，找到最前面的非取消状态的节点，唤醒其中的阻塞线程
+     */
+    private void unparkSuccessor(Node node) {
+        int ws = node.waitStatus;
+        if (ws < 0) {
+            compareAndSetWaitStatus(node, ws, 0);
+        }
+        Node s = node.next;
+        // 是否没有下一个节点或者下一个节点已处于取消状态
+        if (s == null || s.waitStatus > 0) {
+            s = null;
+            // 从tail节点开始向前查找，找到最前面的非取消状态的节点给s节点
+            for (Node t = tail; t != null && t != node; t = t.prev) {
+                if (t.waitStatus <= 0) {
+                    s = t;
+                }
+            }
+        }
+        // 如果存在非取消状态的节点，则取消阻塞对应线程
+        if (s != null) {
+            LockSupport.unpark(s.thread);
+        }
+    }
+
+    /**
+     * （这个方法没有太看懂）
+     * 在共享模式释放，唤醒下一个节点并确保能传递到其他节点
+     */
+    private void doReleaseShared() {
+        for (;;) {
+            Node h = head;
+            // 确保链表中有大于一个节点
+            if (h != null && h != tail) {
+                int ws = h.waitStatus;
+                if (ws == Node.SIGNAL) {
+                    // 如果head节点是SIGNAL状态，那么将其状态改为0
+                    if (!compareAndSetWaitStatus(h, Node.SIGNAL, 0)) {
+                        continue;
+                    }
+                    // 并唤醒下一个节点中的线程
+                    unparkSuccessor(h);
+                } else if (ws == 0 && !compareAndSetWaitStatus(h, 0, Node.PROPAGATE)) {
+                    // 如果是普通同步节点，并且将其设置为PROPAGETE状态失败
+                    continue;
+                }
+            }
+            if (h == head) {
+                break;
+            }
+        }
+    }
+
+    private void setHeadAndPropagate(Node node, int propogate) {
+        Node h = head;
+        setHead(node);
+        if (propogate > 0 || h == null || h.waitStatus < 0 ||
+            (h = head) == null || h.waitStatus < 0) {
+            Node s = node.next;
+            if (s == null || s.isShared()) {
+                doReleaseShared();
+            }
+        }
+    }
+
+    private void cancelAcquire(Node node) {
+        if (node == null) {
+            return;
+        }
+        node.thread = null;
+        Node pred = node.prev;
+        // 从node向前跳过处于取消状态的节点
+        while (pred.waitStatus > 0) {
+            node.prev = pred = pred.prev;
+        }
+
+        Node preNext = pred.next;
+        node.waitStatus = Node.CANCELLED;
+
+        if (node == tail && compareAndSetTail(node, pred)) {
+            compareAndSetNext(pred, predNext, null);
+        } else {
+            int ws;
+            if (pred != head &&
+                ((ws = pred.waitStatus) == Node.SIGNAL ||
+                (ws <= 0 && compareAndSetWaitStatus(pred, ws, Node.SIGNAL))) &&
+                pred.thread != null) {
+                
+                Node next = node.next;
+                if (next != null && next.waitStatus <= 0) {
+                    compareAndSetNext(pred, predNext, next);
+                }
+            } else {
+                unparkSuccessor(node);
+            }
+            node.next = node; // help GC
+        }
+    }
+
+    /**
+     * 获取失败后是否应该阻塞
+     * 如果node节点的前一个节点pred处于SIGNAL状态，则可以阻塞，返回true
+     * 否则返回false
+     */
+    private static boolean shouldParkAfterFailedAcquire(Node pred, Node node) {
+        int ws = pred.waitStatus;
+        if (ws == Node.SIGNAL) {
+            // 说明node前一个节点是SIGNAL状态，其释放时会唤醒node节点，可以放心阻塞node
+            return true;
+        }
+        // node前一个节点处于取消状态
+        if (ws > 0) {
+            do {
+                // 删除node节点之前的所有处于取消状态的节点
+                node.prev = pred = pred.prev;
+            } while (pred.waitStatus > 0);
+            pred.next = node;
+        } else {
+            // 否则将node前一个节点状态设置为 SIGNAL
+            compareAndSetWaitStatus(pred, ws, Node.SIGNAL);
+        }
+        return false;
+    }
+
+    static void selfInterrupt() {
+        Thread.currentThread().interrupt();
+    }
+
+    // 阻塞当前线程并返回当前是否被中断
+    private final boolean parkAndCheckInterrupt() {
+        LockSupport.park(this);
+        return Thread.interrupted();
+    }
+
+    final boolean acquireQueued(final Node node, int arg) {
+        boolean failed = true;
+        try {
+            boolean interrupted = false;
+            for (;;) {
+                final Node p = node.predecessor();
+                if (p == head && tryAcquire(arg)) {
+                    setHead(node);
+                    p.next = null; // help GC
+                    failed = false;
+                    return interrupted;
+                }
+                if (shouldParkAfterFailedAcquire(p, node) &&
+                    parkAndCheckInterrupt()) {
+                    interrupted = true;
+                }
+            }
+        } finally {
+            if (failed) {
+                cancelAcquire(node);
+            }
+        
+    }
+
+    private void doAcquireInterruptibly(int arg) throws interruptedException {
+        final Node node = addWaiter(Node.EXCLUSIVE);
+        boolean failed = true;
+        try {
+            for (;;) {
+                final Node p = node.predecessor();
+                if (p == head && tryAcquire(arg)) {
+                    setHead(node);
+                    p.next = null;
+                    failed = false;
+                    return;
+                }
+                if (shouldParkAfterFailedAcquire(p, node) &&
+                    parkAndCheckInterrupt()) {
+                        throw new interruptedException();
+                    }
+            }
+        } finally {
+            if (failed) {
+                cancelAcquire(node);
+            }
+        }
+    }
+
+    private boolean doAcquireNanos(int arg, long nanosTimeout) throws interruptedException {
+        if (nanosTimeout <= 0L) {
+            return false;
+        }
+        final long deadline = System.nanoTime() + nanosTimeout;
+        final Node node = addWaiter(Node.EXCLUSIVE);
+        boolean failed = true;
+        try {
+            for (;;) {
+                final Node p = node.predecessor();
+                if (p == head && tryAcquire(arg)) {
+                    setHead(node);
+                    p.next = null;
+                    failed = false;
+                    return true;
+                }
+                nanosTimeout = deadline - System.nanoTime();
+                if (nanosTimeout <= 0L) {
+                    return false;
+                }
+                // 自旋阻塞
+                if (shouldParkAfterFailedAcquire(p, node) &&
+                    nanosTimeout > spinForTimeoutThreshold) {
+                    LockSupport.parkNanos(this, nanosTimeout);
+                }
+                if (Thread.interrupted()) {
+                    throws new interruptedException();
+                }
+            }
+        } finally {
+            if (failed) {
+                cancelAcquire(node);
+            }
+        }
+    }
+
+    private void doAcquireShared(int arg) {
+        final Node node = addWaiter(Node.SHARED);
+        boolean failed = true;
+        try {
+            boolean interrupted = false;
+            for (;;) {
+                final Node p = node.predecessor();
+                if (p == head) {
+                    int r = tryAcquireShared(arg);
+                    if (r >= 0) {
+                        setHeadAndPropagate(node, r);
+                        p.next = null;
+                        if (interrupted) {
+                            // 中断当前线程
+                            selfInterrupt();
+                        }
+                        failed = false;
+                        return;
+                    }
+                }
+                if (shouldParkAfterFailedAcquire(p, node) &&
+                    parkAndCheckInterrupt()) {
+                    interrupted = true;
+                }
+            }
+        } finally {
+            if (failed) {
+                cancelAcquire(node);
+            }
+        }
+    }
+
+    private void doAcquireSharedInterruptibly(int arg) throws interruptedException {
+        final Node node = addWaiter(Node.SHARED);
+        boolean failed = true;
+        try {
+            for(;;) {
+                final Node p = node.predecessor();
+                if (p == head) {
+                    int r = tryAcquireShared(arg);
+                    if (r >= 0) {
+                        setHeadAndPropagate(node, r);
+                        p.next = null;
+                        failed = false;
+                        return;
+                    }
+                }
+                if (shouldParkAfterFailedAcquire(p, node) &&
+                    parkAndCheckInterrupt()) {
+                    throw new InterruptedException();
+                }
+            }
+        } finally {
+            if (failed) {
+                cancelAcquire(node);
+            }
+        }
+    }
+
+    private boolean doAcquireSharedNanos(int arg, long nanosTimeout) throws InterruptedException {
+        if (nanosTimeout <= 0L) {
+            return false;
+        }
+        final long deadline = System.nanoTime() + nanosTimeout;
+        final Node node = addWaiter(Node.SHARED);
+        boolean failed = true;
+        try {
+            for (;;) {
+                final Node p = node.predecessor();
+                if (p == head) {
+                    int r = tryAcquireShared(arg);
+                    if (r >= 0) {
+                        setHeadAndPropagate(node, r);
+                        p.next = null;
+                        failed = false;
+                        return true;
+                    }
+                }
+                nanosTimeout = deadline - System.nanoTime();
+                if (nanosTimeout <= 0L) {
+                    return false;
+                }
+                if (shouldParkAfterFailedAcquire(p, node) &&
+                    nanosTimeout > spinForTimeoutThreshold) {
+                    LockSupport.parkNanos(this, nanosTimeout);
+                }
+                if (Thread.interrupted()) {
+                    throw new InterruptedException();
+                }
+            }
+        } finally {
+            if (failed) {
+                cancelAcquire(node);
+            }
+        }
+    }
+
+//========================================================
+    /**
+     * 在独占模式下尝试获取
+     * 获取成功返回true
+     */
+    protected boolean tryAcquire(int arg) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * 在独占模式下修改状态为反映已释放的状态
+     */
+    protected boolean tryRelease(int arg) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * 尝试在非独占模式下获取
+     * 失败返回负数
+     * 通过独占方式被获取返回0
+     * 通过非独占方式被获取返回正数
+     */
+    protected int tryAcquireShared(int arg) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * 在非独占模式下将状态设置为释放状态
+     */
+    protected boolean tryReleaseShared(int arg) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * 如果在独占模式下返回true
+     * 否则返回false
+     */
+    protected boolean isHeldExclusively() {
+        throw new UnsupportedOperationException();
+    }
+
+//========================================================
+
+    /**
+     * 在独占模式下获取，忽略中断
+     * 可以用来实现 Lock.lock 方法
+     */
+    public final void acquire(int arg) {
+        if (! tryAcquire(arg) && acquireQueued(addWaiter(Node.EXCLUSIVE), arg)) {
+            selfInterrupt();
+        }
+    }
+
+    /**
+     * 在独占模式下获取，可以被中断
+     * 可以用来实现 Lock.lockInterruptibly 方法
+     */
+    public final void doAcquireInterruptibly(int arg) throws InterruptedException {
+        if (Thread.interrupted()) {
+            throw new InterruptedException();
+        }
+        if (! tryAcquire(arg)) {
+            doAcquireInterruptibly(arg);
+        }
+    }
+
+    public final boolean tryAcquireNanos(int arg, long nanosTimeout) throws InterruptedException {
+        if (Thread.interrupted()) {
+            throw new InterruptedException();
+        }
+        return tryAcquire(arg) || doAcquireNanos(arg, nanosTimeout);
+    }
+
+    /**
+     * 独占模式下释放
+     * 可以实现 Lock.unlock 方法
+     */
+    public final boolean release(int arg) {
+        if (tryRelease(arg)) {
+            Node h = head;
+            if (h != null && h.waitStatus != 0) {
+                unparkSuccessor(h);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public final void acquireShared(int arg) {
+        if (tryAcquireShared(arg) < 0) {
+            doAcquireShared(arg);
+        }
+    }
+
+    public final void acquireSharedInterruptibly(int arg) throws InterruptedException {
+        if (Thread.interrupted()) {
+            throw new InterruptedException();
+        }
+        if (tryAcquireShared(arg) < 0) {
+            doAcquireInterruptibly(arg);
+        }
+    }
+
+    public final boolean tryAcquireSharedNanos(int arg, long nanosTimeout) throws InterruptedException {
+        if (Thread.interrupted()) {
+            throw new InterruptedException();
+        }
+        return tryAcquireShared(arg) >= 0 || doAcquireSharedInterruptibly(arg, nanosTimeout);
+    }
+
+    public final boolean releaseShared(int arg) {
+        if (tryReleaseShared(arg)) {
+            doReleaseShared();
+            return true;
+        }
+        return false;
+    }
+
+    public final boolean hasQueuedThreads() {
+        return head != tail;
+    }
+
+    /**
+     * 判断是否有线程在争夺此同步器
+     * 即判断是否有线程在获取阻塞阶段
+     */
+    public final boolean hasContended() {
+        return head != null;
+    }
+
+    public final Thread getFirstQueuedThread() {
+        return (head == tail) ? null : fullGetFirstQueuedThread();
+    }
+
+    private Thread fullGetFirstQueuedThread() {
+        Node h, s;
+        Thread st;
+        if (((h = head) != null && (s = h.next) != null &&
+            s.prev == head && (st = s.thread) != null ||
+            ((h = head) != null && (s = h.next) != null &&
+            s.prev == head && (st = s.thread) != null))) {
+            return st;
+        }
+
+        Node t = tail;
+        Thread firstThread = null;
+        while (t != null && t != head) {
+            Thread tt = t.head;
+            if (tt != null) {
+                firstThread = tt;
+            }
+            t = t.prev;
+        }
+        return firstThread;
+    }
+
+    /**
+     * 判断线程是否在队列中
+     */
+    public final boolean isQueued(Thread thread) {
+        if (thred == null) {
+            throw new NullPointerException();
+        }
+        for (Node p = tail; p != null; p = p.prev) {
+            if (p.thread == thread) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    final boolean apparentlyFirstQueuedIsExclusive() {
+        Node h, s;
+        return (h = head) != null &&
+            (s = h.next) != null &&
+            !s.isShared() &&
+            s.thread != null;
+    }
+
+    public final boolean hasQueuedPredecessors() {
+        Node t = tail;
+        Node h = head;
+        Node s;
+        return h != t &&
+            ((s = h.next) == null || s.thread != Thread.currentThread());
+    }
+
+    public final int getQueueLength() {
+        int n = 0;
+        for (Node p = tail; p != null; p = p.prev) {
+            if (p.thread != null) {
+                ++n;
+            }
+        }
+        return n;
+    }
+
+    /**
+     * 如果头结点为空，将其设置为头结点
+     */
+    private final boolean compareAndSetHead(Node update) {
+        return unsafe.compareAndSwapObject(this, headOffset, null, update);
+    }
+
+    private final boolean compareAndSetTail(Node expect, Node update) {
+        return unsafe.compareAndSwapObject(this, tailOffset, expect, update);
+    }
+
+    private static final boolean compareAndSetWaitStatus(Node node, int expect, int update) {
+        return unsafe.compareAndSwapInt(node, waitStatusOffset, expect, update);
+    }
+
+    private static final boolean compareAndSetNext(Node node, Node expect, Node update) {
+        return unsafe.compareAndSwapObject(node, nextOffset, expect, update);
+    }
+
+
+    static final class Node {
+        static final Node SHARED = new Node();
+        static final Node EXCLUSIVE = null;
+
+        static final int CANCELLED = 1;
+        static final int SIGNAL = -1;
+        static final int CONDITION = -2;
+        static final int PROPAGATE = -3;
+
+        volatile int waitStatus;
+        volatile Node prev;
+        volatile Node next;
+        volatile Thread thread;
+
+        Node nextWaiter;
+
+        final boolean isShared() {
+            return nextWaiter == SHARED;
+        }
+
+        final Node predecessor() throws NullPointerException {
+            Node p = prev;
+            if (p == null) {
+                throw new NullPointerException();
+            } else {
+                return p;
+            }
+        }
+
+        Node() {}
+
+        Node(Thread thread, Node mode) {
+            this.nextWaiter = mode;
+            this.thread = thread;
+        }
+
+        Node(Thread thread, int waitStatus) {
+            this.waitStatus = waitStatus;
+            this.thread = thread;
+        }
+    }
+}
